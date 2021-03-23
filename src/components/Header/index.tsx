4aa2ae8813ffa63447b4830d2c571811ddf7ad5d
jgabriel1 <josegabriel@det.ufc.br>
2021-03-23 16:46:12 -0300

    Created github sign in button and styles.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
 export function Header() {
@@ -13,6 +14,8 @@ export function Header() {
 
           <a href="">Posts</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   );
