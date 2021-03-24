b357bd23a8f499e376dab42f8ee307dd3fab8906
jgabriel1 <josegabriel@det.ufc.br>
2021-03-24 17:23:20 -0300

    Added sign in and out logic to the button.

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,38 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
+  const isUserLoggedIn = !!session;
+
+  const handleSignInWithGithub = async () => {
+    await signIn('github');
+  };
+
+  const handleSignOut = async () => {
+    await signOut();
+  };
 
   return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={handleSignOut}
+    >
       <FaGithub color="#04d361" />
-      José Gabriel Mourão
+
+      {session.user.name}
+
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={handleSignInWithGithub}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
