5e41eea327be08481e4b76c29a82735bd41a55d2
jgabriel1 <josegabriel@det.ufc.br>
2021-03-24 23:58:50 -0300

    Added subscription action to the button.

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJS } from '../../services/stripeJS';
 import styles from './styles.module.scss';
 
 type SubscribeButtonProps = {
@@ -5,8 +8,32 @@ type SubscribeButtonProps = {
 };
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  const handleSubscribe = async () => {
+    if (!session) {
+      return signIn('github');
+    }
+
+    try {
+      const response = await api.post<{ sessionId: string }>('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJS();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
